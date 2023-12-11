a3146e3c61fac41e16b72867f301e137159eb800
kevempedro <kevempereira520@gmail.com>
2023-12-11 10:52:20 -0300

    Componentizando os relatórios

---
+++ components/report/script.js
@@ -0,0 +1,243 @@
+import budgetTypesEnum from '../../enums/budgetTypes.enum.js';
+import { getMonthByNumber } from '../../utils.js';
+
+const html = `
+<div class="order-first order-sm-last" style="height: max-content;">
+    <h3>Resultados do ano de {{ yearSelected }}</h3>
+
+    <v-select v-model="yearSelected" :items="years" label="Ano"></v-select>
+
+    <div v-if="resultsOfTheYearHasAnyRegister" class="d-flex flex-column mt-4">
+        <div class="d-flex justify-space-between" style="gap: 20px;">
+            <div v-for="(budget, index) in resultsOfTheYear" :key="index" class="d-flex align-center flex-column">
+                <p class="ma-0 text-center mb-2">
+                    {{ budget.title }}
+                </p>
+
+                <v-progress-circular :rotate="360" :size="100" :width="15"
+                    :value="((budget.total / amountOfTheCurrentYear) * 100)" :color="budget.color">
+                    {{ ((budget.total / amountOfTheCurrentYear) * 100).toFixed(2) }}
+                </v-progress-circular>
+
+                <p class="ma-0 text-center mt-2">
+                    {{ budget.total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) }}
+                </p>
+            </div>
+        </div>
+
+        <h3 class="mt-10">Mês em Destaque</h3>
+
+        <div class="d-flex justify-space-between mt-6" style="gap: 20px;">
+            <div v-for="(element, index) in bestMonths" :key="index"
+                class="month-result-container d-flex align-center flex-column" :class="element.type">
+                <v-tooltip right>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-icon v-bind="attrs" v-on="on">
+                            {{
+                            element.type === budgetTypesEnumData.GAIN ? 'mdi-cash-plus' : ''
+                            }}
+                            {{
+                            element.type === budgetTypesEnumData.INVESTMENT ? 'mdi-piggy-bank-outline' : ''
+                            }}
+                            {{
+                            element.type === budgetTypesEnumData.COST ? 'mdi-cash-minus' : ''
+                            }}
+                        </v-icon>
+                    </template>
+
+                    <span>
+                        {{
+                        element.type === budgetTypesEnumData.GAIN ? 'Ganhos' : ''
+                        }}
+                        {{
+                        element.type === budgetTypesEnumData.INVESTMENT ? 'Investimentos' : ''
+                        }}
+                        {{
+                        element.type === budgetTypesEnumData.COST ? 'Despesas' : ''
+                        }}
+                    </span>
+                </v-tooltip>
+
+                <p class="ma-0 mt-2 text-center">
+                    <strong>{{ getMonthByNumber(element.month) }}</strong> {{ element.total.toLocaleString('pt-BR', {
+                    style: 'currency', currency: 'BRL' }) }}
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <p v-else class="ma-0 text-center mt-2">
+        Nenhum registro encontrado para o ano de {{ yearSelected }}
+    </p>
+</div>
+`
+;
+
+const Report = {
+    name: 'Report',
+    props: {
+        budgetItems: {
+            required: true
+        }
+    },
+    data () {
+        return {
+            years: ['2023', '2024', '2025'],
+            yearSelected: '',
+            currentYear: new Date().getFullYear(),
+            amountOfTheCurrentYear: 0,
+            budgetTypesEnumData: budgetTypesEnum
+        };
+    },
+
+    template: html,
+
+    created() {
+        this.yearSelected = this.currentYear;
+    },
+
+    computed: {
+        resultsOfTheYearHasAnyRegister() {
+            return this.resultsOfTheYear.find(register => register.total > 0);
+        },
+
+        resultsOfTheYear() {
+            const budgetItemsInCurrentYear = this.budgetItems.filter(budget => budget.date.includes(`/${this.yearSelected.toString()}`));
+
+            this.amountOfTheCurrentYear = budgetItemsInCurrentYear.reduce((acc, budget) => {
+                acc += Number(budget.amount)
+
+                return acc;
+            }, 0)
+
+            return [
+                {
+                    title: 'Ganhos',
+                    color: '#4caf50',
+                    total: budgetItemsInCurrentYear.reduce((acc, budget) => {
+                        if (budget.typeBudget === budgetTypesEnum.GAIN) {
+                            acc += Number(budget.amount);
+                        }
+
+                        return acc;
+                    }, 0)
+                },
+                {
+                    title: 'Investimentos',
+                    color: '#2196f3',
+                    total: budgetItemsInCurrentYear.reduce((acc, budget) => {
+                        if (budget.typeBudget === budgetTypesEnum.INVESTMENT) {
+                            acc += Number(budget.amount);
+                        }
+
+                        return acc;
+                    }, 0)
+                },
+                {
+                    title: 'Despesas',
+                    color: '#ff5252',
+                    total: budgetItemsInCurrentYear.reduce((acc, budget) => {
+                        if (budget.typeBudget === budgetTypesEnum.COST) {
+                            acc += Number(budget.amount);
+                        }
+
+                        return acc;
+                    }, 0)
+                }
+            ];
+        },
+
+        bestMonths() {
+            const budgetItemsInCurrentYear = this.budgetItems.filter(budget => budget.date.includes(`/${this.yearSelected.toString()}`));
+
+            const budgetItemsGroupedByDate = budgetItemsInCurrentYear.reduce((acc, obj) => {
+                const found = acc.find(item => item.date.split('/')[0] === obj.date.split('/')[0]);
+
+                if (found) {
+                  found.data.push(obj);
+                } else {
+                  acc.push({ date: obj.date.split('/')[0], data: [obj] });
+                }
+
+                return acc;
+            }, []);
+
+            const gain = [];
+            const investment = [];
+            const cost = [];
+
+            budgetItemsGroupedByDate.forEach(element => {
+                const total = element.data.reduce((acc, budget) => {
+                    if (budget.typeBudget === budgetTypesEnum.GAIN) {
+                        acc += Number(budget.amount);
+                    }
+
+                    return acc;
+                }, 0);
+
+                gain.push({
+                    type: budgetTypesEnum.GAIN,
+                    month: element.date,
+                    total
+                });
+            });
+
+            budgetItemsGroupedByDate.forEach(element => {
+                const total = element.data.reduce((acc, budget) => {
+                    if (budget.typeBudget === budgetTypesEnum.INVESTMENT) {
+                        acc += Number(budget.amount);
+                    }
+
+                    return acc;
+                }, 0);
+
+                investment.push({
+                    type: budgetTypesEnum.INVESTMENT,
+                    month: element.date,
+                    total
+                });
+            });
+
+            budgetItemsGroupedByDate.forEach(element => {
+                const total = element.data.reduce((acc, budget) => {
+                    if (budget.typeBudget === budgetTypesEnum.COST) {
+                        acc += Number(budget.amount);
+                    }
+
+                    return acc;
+                }, 0);
+
+                cost.push({
+                    type: budgetTypesEnum.COST,
+                    month: element.date,
+                    total
+                });
+            });
+
+            return [
+
+                gain.reduce((previous, current) => {
+                    return parseFloat(previous.total) > parseFloat(current.total) ? previous : current;
+                }),
+
+                investment.reduce((previous, current) => {
+                    return parseFloat(previous.total) > parseFloat(current.total) ? previous : current;
+                }),
+
+                cost.reduce((previous, current) => {
+                        return parseFloat(previous.total) > parseFloat(current.total) ? previous : current;
+                })
+            ];
+        }
+    },
+
+    methods: {
+        getMonthByNumber(month) {
+            const monthNumber = Number(month.replace('0', '')) - 1;
+
+            return getMonthByNumber(monthNumber);
+        },
+    }
+};
+
+export default Report;
--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 import { getDatabase, ref, child, get, set, update, remove  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js';
 
-import { getMonthByNumber, getFullMonthByNumber } from './utils.js';
+import { getFullMonthByNumber } from './utils.js';
 import budgetTypesEnum from './enums/budgetTypes.enum.js';
+
+import Report from './components/report/script.js';
 
 new Vue({
     el: '#app',
     vuetify: new Vuetify(),
+
+    components: {
+        'report-component': Report
+    },
 
     data () {
         return {
@@ -30,10 +36,7 @@ new Vue({
             loadingRegisterBudget: false,
             loadingUpdateBudget: false,
             loadingDeleteBudget: false,
-            amountOfTheCurrentYear: 0,
-            currentYear: new Date().getFullYear(),
             years: ['2023', '2024', '2025'],
-            yearSelected: '',
             showSnackbarError: false,
             snackbarErrorText: '',
             mdSize: '12',
@@ -45,7 +48,6 @@ new Vue({
     },
 
     created() {
-        this.yearSelected = this.currentYear;
         this.getBudgetItems();
     },
 
@@ -207,149 +209,10 @@ new Vue({
             }
         },
 
-        resultsOfTheYear() {
-            const budgetItemsInCurrentYear = this.budgetItems.filter(budget => budget.date.includes(`/${this.yearSelected.toString()}`));
-
-            this.amountOfTheCurrentYear = budgetItemsInCurrentYear.reduce((acc, budget) => {
-                acc += Number(budget.amount)
-
-                return acc;
-            }, 0)
-
-            return [
-                {
-                    title: 'Ganhos',
-                    color: '#4caf50',
-                    total: budgetItemsInCurrentYear.reduce((acc, budget) => {
-                        if (budget.typeBudget === budgetTypesEnum.GAIN) {
-                            acc += Number(budget.amount);
-                        }
-
-                        return acc;
-                    }, 0)
-                },
-                {
-                    title: 'Investimentos',
-                    color: '#2196f3',
-                    total: budgetItemsInCurrentYear.reduce((acc, budget) => {
-                        if (budget.typeBudget === budgetTypesEnum.INVESTMENT) {
-                            acc += Number(budget.amount);
-                        }
-
-                        return acc;
-                    }, 0)
-                },
-                {
-                    title: 'Despesas',
-                    color: '#ff5252',
-                    total: budgetItemsInCurrentYear.reduce((acc, budget) => {
-                        if (budget.typeBudget === budgetTypesEnum.COST) {
-                            acc += Number(budget.amount);
-                        }
-
-                        return acc;
-                    }, 0)
-                }
-            ];
-        },
-
-        bestMonths() {
-            const budgetItemsInCurrentYear = this.budgetItems.filter(budget => budget.date.includes(`/${this.yearSelected.toString()}`));
-
-            const budgetItemsGroupedByDate = budgetItemsInCurrentYear.reduce((acc, obj) => {
-                const found = acc.find(item => item.date.split('/')[0] === obj.date.split('/')[0]);
-
-                if (found) {
-                  found.data.push(obj);
-                } else {
-                  acc.push({ date: obj.date.split('/')[0], data: [obj] });
-                }
-
-                return acc;
-            }, []);
-
-            const gain = [];
-            const investment = [];
-            const cost = [];
-
-            budgetItemsGroupedByDate.forEach(element => {
-                const total = element.data.reduce((acc, budget) => {
-                    if (budget.typeBudget === budgetTypesEnum.GAIN) {
-                        acc += Number(budget.amount);
-                    }
-
-                    return acc;
-                }, 0);
-
-                gain.push({
-                    type: budgetTypesEnum.GAIN,
-                    month: element.date,
-                    total
-                });
-            });
-
-            budgetItemsGroupedByDate.forEach(element => {
-                const total = element.data.reduce((acc, budget) => {
-                    if (budget.typeBudget === budgetTypesEnum.INVESTMENT) {
-                        acc += Number(budget.amount);
-                    }
-
-                    return acc;
-                }, 0);
-
-                investment.push({
-                    type: budgetTypesEnum.INVESTMENT,
-                    month: element.date,
-                    total
-                });
-            });
-
-            budgetItemsGroupedByDate.forEach(element => {
-                const total = element.data.reduce((acc, budget) => {
-                    if (budget.typeBudget === budgetTypesEnum.COST) {
-                        acc += Number(budget.amount);
-                    }
-
-                    return acc;
-                }, 0);
-
-                cost.push({
-                    type: budgetTypesEnum.COST,
-                    month: element.date,
-                    total
-                });
-            });
-
-            return [
-
-                gain.reduce((previous, current) => {
-                    return parseFloat(previous.total) > parseFloat(current.total) ? previous : current;
-                }),
-
-                investment.reduce((previous, current) => {
-                    return parseFloat(previous.total) > parseFloat(current.total) ? previous : current;
-                }),
-
-                cost.reduce((previous, current) => {
-                        return parseFloat(previous.total) > parseFloat(current.total) ? previous : current;
-                })
-            ];
-        },
-
-        getMonthByNumber(month) {
-            const monthNumber = Number(month.replace('0', '')) - 1;
-
-            return getMonthByNumber(monthNumber);
-        },
-
         getFullMonthByNumber(month) {
             const monthNumber = Number(month.replace('0', '')) - 1;
 
             return getFullMonthByNumber(monthNumber);
-        },
-
-        resultsOfTheYearHasAnyRegister() {
-            return this.resultsOfTheYear().find(register => register.total > 0);
         },
 
         getBudgetItems() {
@@ -357,7 +220,6 @@ new Vue({
             .then((snapshot) => {
                 if (snapshot.exists()) {
                     this.budgetItems = snapshot.val();
-                    console.log('aqui -> ', this.budgetItems)
                     this.filterBudgetItems();
 
                     this.setPagination;
